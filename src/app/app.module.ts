e2cc949270951dbc86097da32c87a324f866d53c
Selmene <enemles.ihraf@gmail.com>
2023-04-01 12:36:43 +0200

    Add angularfire + environment.ts file

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,17 @@
-import { NgModule } from '@angular/core';
+import {enableProdMode, NgModule} from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import {SharedModule} from './shared/shared.module';
+import { environment } from '../environments/environment';
+import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
+import { provideAuth,getAuth } from '@angular/fire/auth';
+import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+
+if (environment.production) {
+  enableProdMode();
+}
 
 @NgModule({
   declarations: [
@@ -12,7 +20,10 @@ import {SharedModule} from './shared/shared.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    SharedModule
+    SharedModule,
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore())
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/environments/environment.ts
@@ -0,0 +1,21 @@
+export const environment = {
+  firebase: {
+    projectId: 'fut5-72da7',
+    appId: '1:1032115863868:web:745378c081731dec8a5e39',
+    storageBucket: 'fut5-72da7.appspot.com',
+    apiKey: 'AIzaSyBDrT1n3ht4JVIu5TrpMNwXzjLCaM1dU8g',
+    authDomain: 'fut5-72da7.firebaseapp.com',
+    messagingSenderId: '1032115863868',
+    measurementId: 'G-3CJQLPRRNF',
+  },
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBDrT1n3ht4JVIu5TrpMNwXzjLCaM1dU8g",
+    authDomain: "fut5-72da7.firebaseapp.com",
+    projectId: "fut5-72da7",
+    storageBucket: "fut5-72da7.appspot.com",
+    messagingSenderId: "1032115863868",
+    appId: "1:1032115863868:web:745378c081731dec8a5e39",
+    measurementId: "G-3CJQLPRRNF"
+  }
+};
