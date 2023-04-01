9d02fcdbdcd1ecbc507d4e5ea2c9920f7ff4ebb7
Blewkse <julesbloux@gmail.com>
2023-04-02 01:37:36 +0200

    début player card

---
+++ src/app/shared/player-card/player-card.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-player-card',
+  templateUrl: './player-card.component.html',
+  styleUrls: ['./player-card.component.scss']
+})
+export class PlayerCardComponent {
+
+  perf: Number = 99;
+  nickname: String = "L'atout";
+
+}
