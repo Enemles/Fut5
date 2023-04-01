2a863c14e2e786437bed1227963e35782443c362
Selmene <enemles.ihraf@gmail.com>
2023-04-01 12:43:10 +0200

    app.module components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,14 @@ import { environment } from '../environments/environment';
 import { initializeApp,provideFirebaseApp } from '@angular/fire/app';
 import { provideAuth,getAuth } from '@angular/fire/auth';
 import { provideFirestore,getFirestore } from '@angular/fire/firestore';
+import { HomeComponent } from './home/home.component';
+import { MatchListComponent } from './match-list/match-list.component';
+import { MatchDetailsComponent } from './match-details/match-details.component';
+import { TeamListComponent } from './team-list/team-list.component';
+import { TeamDetailsComponent } from './team-details/team-details.component';
+import { PlayerDetailsComponent } from './player-details/player-details.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
 
 if (environment.production) {
   enableProdMode();
@@ -15,7 +23,15 @@ if (environment.production) {
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    MatchListComponent,
+    MatchDetailsComponent,
+    TeamListComponent,
+    TeamDetailsComponent,
+    PlayerDetailsComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule,
