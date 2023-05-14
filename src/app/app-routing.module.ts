664afdbcc83aeb1cc179772bcd12b49bf63fe74a
Blewkse <julesbloux@gmail.com>
2023-05-14 23:48:21 +0200

    ajout lien vers profile pour pres

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,12 +8,14 @@ import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import {AuthGuard} from "./auth.guard";
 import {TooltipPlayerComponent} from "./profile/tooltip-player/tooltip-player.component";
+import {ProfileComponent} from "./profile/profile.component";
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'matches', component: MatchListComponent , canActivate: [AuthGuard]},
   { path: 'matches/:id', component: MatchDetailsComponent },
   { path: 'players/:id', component: PlayerDetailsComponent },
+  { path: 'profile', component: ProfileComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
   { path: 'tooltip', component: TooltipPlayerComponent}
